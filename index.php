c4e262dd5bdf7de7c381ebd9046b6b973e9050f6
Rommy1488 <rommy1054@gmail.com>
2026-02-11 19:04:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+﻿<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+$logFile = __DIR__ . '/clicks.txt';
+$date = date('d/m/Y');
+
+if (!file_exists($logFile)) {
+    file_put_contents($logFile, '');
+}
+
+$content = file($logFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$found = false;
+
+foreach ($content as $key => $line) {
+    if (strpos($line, $date) === 0) {
+        $parts = explode(' : ', $line);
+        $count = (int)$parts[1] + 1;
+        $content[$key] = "$date : $count";
+        $found = true;
+        break;
+    }
+}
+
+if (!$found) {
+    $content[] = "$date : 1";
+}
+
+file_put_contents($logFile, implode("\n", $content) . "\n");
+?>
